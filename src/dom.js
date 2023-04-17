fb7aa3f18c5ff556611eb88d0af72b359fc8a25d
joerhew <joerhew@gmail.com>
2023-04-17 17:16:40 -0400

    Moved skeletal DOM structure to index.html from dom.js

--- src/dom.js
+++ src/dom.js
@@ -6,66 +6,45 @@ export const DOM = {
   STORAGE: null,
 
   CONTENT: document.querySelector('#content'),
-  HEADER: document.createElement('div'),
-  
-  CNTR_PROJECTS: document.createElement('div'),
-  BTN_NEW_PROJECT: document.createElement('button'),
-  CURRENT_PROJECTS: document.createElement('div'),
-  
-  CNTR_TASKS: document.createElement('div'),
-  BTN_NEW_TASK: document.createElement('button'),
-  CURRENT_TASKS: document.createElement('div'),
+  HEADER: document.querySelector('#header'),
+  CNTR_PROJECTS: document.querySelector('#cntr-projects'),
+  CURRENT_PROJECTS: document.querySelector('.current-projects'),
+  CNTR_TASKS: document.querySelector('#cntr-tasks'),
+  CURRENT_TASKS: document.querySelector('.current-tasks'),
 
-  MODAL: document.createElement('section'),
-  OVERLAY: document.createElement('div'),
-  FORM_NEW_PROJECT: document.createElement('form'),
-  FORM_NEW_TASK: document.createElement('form'),
+  FORM_PROJECT: document.querySelector("form[name='project']"),
+  FORM_TASK: document.querySelector("form[name='task']"),
+
+  MODAL: document.querySelector('#modal'),
+  OVERLAY: document.querySelector('#overlay'),
+
+  BTN_NEW_PROJECT: document.createElement('button'),
+  BTN_NEW_TASK: document.createElement('button'),
   
   init() {
     //Create an instance of local storage
     this.STORAGE = new Storage();
+    
+    //Populate projects list
+    Object.values(this.STORAGE.projectList).forEach(project => {
+      const existingProjectDiv = document.createElement('div');
+      existingProjectDiv.classList.add('existing-project');
+      existingProjectDiv.innerText = project._name;
+      this.CURRENT_PROJECTS.appendChild(existingProjectDiv);
+    })
+    
+    //Populate tasks list
+    //TBC
 
-    //Add skeleton structure to page
-    this.HEADER.classList.add('header');
-    this.CNTR_PROJECTS.classList.add('container');
-    this.CURRENT_PROJECTS.classList.add('current-projects');
-    this.CNTR_TASKS.classList.add('container');
-    this.CURRENT_TASKS.classList.add('current-tasks');
-
+    //Generate and append buttons
+    this.BTN_NEW_PROJECT.innerText = "Add Project";  
+    this.BTN_NEW_TASK.innerText = "Add Task";
     this.BTN_NEW_PROJECT.id = 'btn_new_project';
     this.BTN_NEW_TASK.id = 'btn_new_task';
-
-    this.MODAL.classList.add('modal', 'hidden');
-    this.OVERLAY.classList.add('overlay', 'hidden');
-    this.FORM_NEW_PROJECT.classList.add('form_new_project', 'hidden');
-    this.FORM_NEW_TASK.classList.add('form_new_task', 'hidden');
-    
-    //Populate header, projects list, and tasks list
-    this.HEADER.innerText = 'My Task Tracker!';
-    this.CURRENT_PROJECTS.innerText = this.STORAGE.projectList;
-    
-    //Create a function to display existing projects
-    console.log(this.STORAGE.projectList);
-
-    this.CURRENT_TASKS.innerText = 'This is where the tasks go';
-    this.BTN_NEW_PROJECT.innerText = "Add Project";
-  
-    this.BTN_NEW_TASK.innerText = "Add Task";
-
-    //Append
-    this.CONTENT.appendChild(this.HEADER);
-    this.CONTENT.appendChild(this.MODAL);
-    this.CONTENT.appendChild(this.OVERLAY);
-    this.CONTENT.appendChild(this.CNTR_PROJECTS);
     this.CNTR_PROJECTS.appendChild(this.BTN_NEW_PROJECT);
-    this.CNTR_PROJECTS.appendChild(this.CURRENT_PROJECTS);
-    this.CONTENT.appendChild(this.CNTR_TASKS);
     this.CNTR_TASKS.appendChild(this.BTN_NEW_TASK);
-    this.CNTR_TASKS.appendChild(this.CURRENT_TASKS);
-    this.MODAL.appendChild(this.FORM_NEW_TASK);
-    this.MODAL.appendChild(this.FORM_NEW_PROJECT);
 
-    //Add a click handler to entire document
+   //Add a click handler to entire document
     document.getElementById('content').addEventListener('click', (event) => {
       const targetElement = event.target;
 
@@ -108,17 +87,17 @@ export const DOM = {
     this.OVERLAY.classList.toggle('hidden');
   
     // Hide all forms first
-    this.FORM_NEW_TASK.classList.add('hidden');
-    this.FORM_NEW_PROJECT.classList.add('hidden');
+    this.FORM_TASK.classList.add('hidden');
+    this.FORM_PROJECT.classList.add('hidden');
   
     // Show the form based on the type parameter
     if (type === 'task') {
-      this.FORM_NEW_TASK.classList.remove('hidden');
+      this.FORM_TASK.classList.remove('hidden');
     } else if (type === 'project') {
-      this.FORM_NEW_PROJECT.classList.remove('hidden');
+      this.FORM_PROJECT.classList.remove('hidden');
 
       //Generate a form to add a new project
-      this.FORM_NEW_PROJECT.innerHTML = '';
+      this.FORM_PROJECT.innerHTML = '';
       
       let InputProjectName = document.createElement('input');
       let LabelProjectName = document.createElement('label');
@@ -138,12 +117,12 @@ export const DOM = {
       buttonAdd.innerText = 'Add';
       buttonCancel.innerText = 'Cancel';
 
-      this.FORM_NEW_PROJECT.appendChild(LabelProjectName);
-      this.FORM_NEW_PROJECT.appendChild(InputProjectName);
-      this.FORM_NEW_PROJECT.appendChild(buttonAdd);
-      this.FORM_NEW_PROJECT.appendChild(buttonCancel);
+      this.FORM_PROJECT.appendChild(LabelProjectName);
+      this.FORM_PROJECT.appendChild(InputProjectName);
+      this.FORM_PROJECT.appendChild(buttonAdd);
+      this.FORM_PROJECT.appendChild(buttonCancel);
 
-      this.FORM_NEW_PROJECT.addEventListener('submit', (event) => {
+      this.FORM_PROJECT.addEventListener('submit', (event) => {
         const PROJECT_NAME = InputProjectName.value;
         const NEW_PROJECT = new Project(PROJECT_NAME);
         
